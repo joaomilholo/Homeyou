5bc05b66d349dae826978057d7eb0102aa91f27b
Joao Milholo <joaomilholo12@gmail.com>
2026-03-28 12:57:31 -0300

    feat: adicionar componente navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+export default function Navbar() {
+  return (
+    <nav className="flex w-304 justify-between mt-6 mx-auto">
+      <div className="flex gap-0.5 text-(--brand-color) items-center">
+        <span className="">&#x2726;</span>
+        <h2 className="text-(--dark-10) text-2xl font-normal font-[Playfair]">
+          Homeyou
+        </h2>
+        <span>&#x2726;</span>
+      </div>
+
+      <div className="flex gap-6 items-center font-[Outfit] text-base font-normal">
+        <a href="#">Início</a>
+        <a href="#">Sobre</a>
+        <a href="#">Galeria</a>
+        <a href="#" className="py-3 px-6 text-white bg-(--brand-color)">
+          Contato
+        </a>
+      </div>
+    </nav>
+  );
+}
